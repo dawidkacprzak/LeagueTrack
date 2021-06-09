fb49e78aa1b9467c75e4f903062e757463a10ae6
Dawid Kacprzak <dawidkacprzak@icloud.com>
2021-06-09 10:46:55 +0200

    Update IntegrationConfiguration.cs

--- src/Test.Integration/IntegrationConfiguration.cs
+++ src/Test.Integration/IntegrationConfiguration.cs
@@ -1,7 +1,8 @@
-﻿namespace Test.Integration
+using System;
+namespace Test.Integration
 {
     public static class IntegrationConfiguration
     {
-        public const string API_KEY = "<KEY>";
+        public const string API_KEY = Environment.GetEnvironmentVariable("KEY");
     }
 }
